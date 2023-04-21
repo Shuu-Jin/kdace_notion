02b7dbefc8cab3b8c184c678e4c47653038a0594
Shuu-Jin <131423723+Shuu-Jin@users.noreply.github.com>
2023-04-21 13:18:31 +0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -51,15 +51,15 @@ export default siteConfig({
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
   navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  navigationStyle: 'custom',
+  navigationLinks: [
+  {
+      title: 'About',
+      pageId: 'f15ade45507e44548decbfc2d0a8b51a'
+  },
+  {
+       title: 'Contact',
+       pageId: '7e3ef891f43b4090bf2eb9abeda27947'
+     }
+  ]
 })
