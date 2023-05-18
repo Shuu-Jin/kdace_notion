f76b801f45001921bead751e9ab32a3746105b3c
Shuu-Jin <131423723+Shuu-Jin@users.noreply.github.com>
2023-05-18 16:00:15 +0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -18,7 +18,7 @@ export default siteConfig({
 
   // social usernames (optional)
   twitter: 'ene_0911',
-  github: 'shuu-jin',
+  //github: 'shuu-jin',//
   //linkedin: 'fisch2',//
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
